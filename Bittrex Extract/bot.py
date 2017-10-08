096b61e1625fafb5e83c00c78533bcd4fdf15a7e
Shoko19961 <Davring@live.de>
2017-10-08 22:14:15 +0200

    Wallet

--- Bittrex Extract/bot.py
+++ Bittrex Extract/bot.py
@@ -23,14 +23,15 @@ class Hist_element:
         return str(self.__dict__)
 
 class Out_element1:
-    def __init__(self,minutes, trans_amount, coin_amount, change_sm, change_md, change_lg, akt_price):
+    def __init__(self,minutes, price, trans_amount, coin_amount, change_sm, change_md, change_lg, t0_price):
         self.minutes = minutes
+        self.price = price
         self.trans_amount = trans_amount
         self.coin_amount = coin_amount
         self.change_sm = change_sm
         self.change_md = change_md
         self.change_lg = change_lg
-        self.akt_price = akt_price
+        self.t0_price = t0_price
 
     def getIterable(self):
         return [self.minutes, self.trans_amount, self.coin_amount, self.change_sm, self.change_md, self.change_lg, self.akt_price]
@@ -39,8 +40,8 @@ class Out_element1:
 CHANGE_SM = 5*60 #seconds
 CHANGE_MD = 15*60 #seconds
 CHANGE_LG = 30*60 #seconds
-HISTORY = 30*60 #size of history store in seconds
-
+HISTORY = 60*60 #size of history store in seconds
+OFFSET = 30*60 #time for future prediction
 #vars + init
 my_bitterx = Bittrex(None, None)
 history_store = [] #storage for history
@@ -83,6 +84,8 @@ def hist_fetch(a):
 def brain_result(b):
     global history_store
     print("Thread 2 started")
+    for element in history_store:
+        element["Flag"] = False
     while True:
         print("I am working")
         for store in history_store:
@@ -90,22 +93,25 @@ def brain_result(b):
                 print("Output steht bevor")
                 coin_count, transactions, changesm, changemd, changelg = 0, 0, 0, 0, 0
                 akt_value = my_bitterx.get_ticker(store["market"])['result']['Last']
-                print(akt_value)
+                offset_value
+                for element in store["history"]:
+                    if (datetime.utcnow().timestamp() - element.ts.timestamp()) < OFFSET:
+                        offset_value = element.price
                 akt_time = datetime.utcnow().hour * 60 + datetime.utcnow().minute
-                changelg = akt_value - store["history"][0].price
+                changelg = offset_value - store["history"][0].price
                 lock.acquire()
                 for counter, element in enumerate(store["history"]):
-                    time_diff = datetime.utcnow().timestamp() - store["history"][counter].ts.timestamp()
-                    if time_diff < CHANGE_SM:
+                    time_diff = datetime.utcnow().timestamp() - element.ts.timestamp()
+                    if time_diff < (CHANGE_SM + OFFSET) and time_diff > OFFSET:
                         if changesm == 0:
-                            changesm = (akt_value - element.price) / element.price
+                            changesm = (offset_value - element.price) / element.price
                         transactions += 1
                         coin_count += element.amount
                     else:
-                        if time_diff < CHANGE_MD and changemd == 0:
-                            changemd = (akt_value - element.price) / element.price
+                        if time_diff < (CHANGE_MD + OFFSET) and time_diff > OFFSET and changemd == 0:
+                            changemd = (offset_value - element.price) / element.price
                 lock.release()
-                out = Out_element1(akt_time, transactions, coin_count, changesm, changemd, changelg, akt_value)
+                out = Out_element1(akt_time, offset_value, transactions, coin_count, changesm, changemd, changelg, akt_value)
                 with open("brain_out.csv", "a") as brain_out:
                     csv_writer = csv.writer(brain_out, delimiter=';', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                     csv_writer.writerow(out.getIterable())
