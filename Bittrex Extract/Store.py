54e7776d50d4f368e4d7b641f25097cb03a0acaa
Shoko19961 <Davring@live.de>
2017-11-09 16:21:10 +0100

    Fehlerbehebung, geänderte Definition

--- Bittrex Extract/Store.py
+++ Bittrex Extract/Store.py
@@ -1,7 +1,7 @@
 import threading
 
-import datetime
-
+from datetime import datetime
+import time
 import array
 from bittrex import Bittrex
 
@@ -21,9 +21,9 @@ class Hist_element:
 
 
 class Store(threading.Thread):
-    def __init__(self, market_currency, base_currency, HISTORY_LEN):
-        self.marketCurrency = market_currency
-        self.baseCurrency = base_currency
+    def __init__(self, market, MARKET_VOLUME, HISTORY_LEN):
+        self.market = market
+        self.MARKET_VOLUME = MARKET_VOLUME
         self.my_bittrex = Bittrex(None, None)
         self.history = []
         self.flag = False
@@ -31,39 +31,43 @@ class Store(threading.Thread):
         self.HISTORY_LEN = HISTORY_LEN
         threading.Thread.__init__(self)
 
-    def getMarket(self) -> str:
-        return self.baseCurrency + "-" + self.marketCurrency
+    def __str__(self):
+        return str(self.__dict__)
 
     def getHistory(self) -> array:
         return self.history
 
     def run(self):
         while True:
-            new_history = self.my_bittrex.get_market_history(self.getMarket())["result"]
-            if new_history:
-                new_history.reverse()
-                self.lock.acquire()
-                for element in new_history:
-                    try:
-                        element = Hist_element(element['Id'], element['Quantity'], element['Price'],
-                                               datetime.strptime(element['TimeStamp'],
-                                                                 '%Y-%m-%dT%H:%M:%S.%f'))  # get required fields
-                        # print(element)
-                    except ValueError:
-                        element = Hist_element(element['Id'], element['Quantity'], element['Price'],
-                                               datetime.strptime(element['TimeStamp'],
-                                                                '%Y-%m-%dT%H:%M:%S'))  # get required fields
-                    if element not in self.history:
-                        self.history.append(element)  # add History Element into the Store
-                print(self.getMarket() + ": " + str(len(self.history)))
-                self.lock.release()
-                self.removeOld()
+            try:
+                new_history = self.my_bittrex.get_market_history(self.market, 200)["result"]
+                if new_history:
+                    new_history.reverse()
+                    self.lock.acquire()
+                    for element in new_history:
+                        try:
+                            element = Hist_element(element['Id'], element['Quantity'], element['Price'],
+                                                   datetime.strptime(element['TimeStamp'],
+                                                                     '%Y-%m-%dT%H:%M:%S.%f'))  # get required fields
+                            # print(element)
+                        except ValueError:
+                            element = Hist_element(element['Id'], element['Quantity'], element['Price'],
+                                                   datetime.strptime(element['TimeStamp'],
+                                                                    '%Y-%m-%dT%H:%M:%S'))  # get required fields
+                        if element not in self.history:
+                            self.history.append(element)  # add History Element into the Store
+                    #print(self.getMarket() + ": " + str(len(self.history)))
+                    self.lock.release()
+                    self.removeOld()
+                    print("Anfrage Abgeschlossen: "+self.market)
+                    time.sleep(5)
+            except:
+                print("Fehler bei History-abfrage: "+self.market)
 
     def removeOld(self):
         if len(self.history) > 0:
             self.lock.acquire()
             while (datetime.utcnow().timestamp() - self.history[0].ts.timestamp()) > self.HISTORY_LEN:
-                self.history["flag"] = True
                 self.history.pop(0)
                 self.flag = True
                 if len(self.history) == 0:
